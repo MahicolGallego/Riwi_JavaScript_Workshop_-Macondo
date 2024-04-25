247f71be9a07fda46a68707ba3261c83f6f27e16
Mahi_G13 <maicolgallego14@gmail.com>
2024-04-25 15:56:02 -0500

    WIP: Case 2 and 3 done

--- challenge_macondo.js
+++ challenge_macondo.js
@@ -10,35 +10,124 @@ const productsSecondAirport = [
 
 let hildebrandoMoney = 2500000;
 
-let userAction;
+//Primera parte case 1
+
+let userActionFirstOptionToEat;
 do {
-  userAction = prompt(
+  userActionFirstOptionToEat = prompt(
     "Estas en el aeropuerto el dorado. ¿Que deseas hacer?\n1. Comprar comida.\n2. Comprar comida en medellin"
   ).trim();
-} while (userAction !== "1" && userAction !== "2");
+} while (userActionFirstOptionToEat !== "1" && userActionFirstOptionToEat !== "2");
 
-if (userAction === "2") {
+//Si selecciona no comer
+
+if (userActionFirstOptionToEat === "2") {
   alert("Ok puedes comprar productos en medellin...");
 } else {
+  //Si selecciona comer
+
   let message = "";
-  const indexProductsAlllow = [];
+  const indexProductsAllow = [];
   productsFirstAirport.forEach((element, index) => {
     message =
       message +
       `\n${index + 1}. Producto: ${element.product}\nPrecio: $${element.price}`;
-    indexProductsAlllow.push(index);
+    indexProductsAllow.push(index);
   });
 
   let purchasedProduct;
 
   do {
-    purchasedProduct = prompt(`Lista de productos: ${message}`).trim();
-  } while (!indexProductsAlllow.includes(purchasedProduct - 1));
+    purchasedProduct = Number(prompt(`Lista de productos: ${message}`).trim());
+  } while (!indexProductsAllow.includes(purchasedProduct - 1));
 
-  hildebrandoMoney =
-    hildebrandoMoney - productsFirstAirport[purchasedProduct - 1].price;
-  alert(`Compra realizada\nsaldo actual = $${hildebrandoMoney}`);
+  hildebrandoMoney -= productsFirstAirport[purchasedProduct - 1].price;
+  alert(`Compra realizada\nDinero actual = $${hildebrandoMoney}`);
 
   if (purchasedProduct - 1 === 0) alert("Levas mucho en el stand y te cae mal");
   else alert("Buena eleccion. Vas lleno y bien");
 }
+
+// Case 2
+
+alert(
+  "llegas a la sala de espera, y tu maleta no cumple con el tamaño de maleta de mano permitido.\n\nPermitidos\nAlto: 55cm\nLargo: 40cm\nAncho: 20cm\n\nTu maleta\nAlto: 60cm\nLargo: 40cm\nAncho: 20cm"
+);
+
+const heigthSuitcase = 60;
+const widthSuitcase = 20;
+const largeSuitcase = 40;
+
+const heigthAllowed = 55;
+const widthAllowed = 20;
+const largeAllowed = 40;
+
+const reductionFactorHeigth = heigthAllowed / heigthSuitcase;
+const reductionFactorWidth = widthAllowed / widthSuitcase;
+const reductionFactorLarge = largeAllowed / largeSuitcase;
+
+// Factor de reducción mas pequeño
+
+const generalReductionFActor = Math.min(
+  reductionFactorHeigth,
+  reductionFactorLarge,
+  reductionFactorWidth
+);
+
+//Calcular las nuevas dimensiones para la maleta
+
+const newHeigthSuitcase = Number((heigthSuitcase * generalReductionFActor).toFixed(2));
+const newWidthSuitcase = Number((widthSuitcase * generalReductionFActor).toFixed(2));
+const newLargeSuitcase = Number((largeSuitcase * generalReductionFActor).toFixed(2));
+
+alert(
+  `Debes sacar elementos hasta que la maleta tenga minimo las siguientes nuevas dimensiones:\n\nAlto: ${newHeigthSuitcase}cm\nLargo: ${newLargeSuitcase}cm\nAncho: ${newWidthSuitcase}cm`
+);
+
+//Continuacion case 1 si hildebrando no compro comida en el dorado
+
+alert("Llegas a medellin")
+
+if(userActionFirstOptionToEat === "2"){
+
+  let message = "";
+  const indexProductsAllow = [];
+  productsSecondAirport.forEach((element, index) => {
+    message =
+      message +
+      `\n${index + 1}. Producto: ${element.product}\nPrecio: $${element.price}`;
+    indexProductsAllow.push(index);
+  });
+
+  let purchasedProduct;
+
+  do {
+    purchasedProduct = Number(prompt(`No consumiste alimentos en el dorado, debes comer algo.\n\nLista de productos: ${message}`).trim());
+  } while (!indexProductsAllow.includes(purchasedProduct - 1));
+
+  hildebrandoMoney -= productsSecondAirport[purchasedProduct-1].price;
+
+  alert(`Compra realizada\nDinero actual = $${hildebrandoMoney}`);
+
+}
+
+// Case 3
+
+alert("Te encuentras con el problema de que el wifi del aeropuerto está fallando, y necesitas asegurar tu reserva en el hotel para el siguiente día, caminando por el aeropuerto encuentra wifi, la red se llama \" ElPassEs: 01110010_01101001_01110111_01101001\", pero la contraseña está cifrada en números binarios, además cobran 50.000 la hora.")
+
+const encryptedKey = "01110010_01101001_01110111_01101001";
+
+const binaryPartition = encryptedKey.split("_").map(element => parseInt(element, 2)
+)
+
+let decryptedKey = "";
+
+console.log(binaryPartition)
+
+binaryPartition.forEach(element => {
+  decryptedKey += String.fromCharCode(element)
+})
+
+hildebrandoMoney = hildebrandoMoney - 50000;
+
+alert(`Descifrando la clave...\n\nAl fin, la clave es: ${decryptedKey}\nHay que descifrar la clave y encima pagar $50.000\nDinero actual = $${hildebrandoMoney}`)
